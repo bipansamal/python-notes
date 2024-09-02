326cc02ffadc05d2fae25731f1ab74f3ecd6fa6b
bipanmagar <bipanmagar69@gmail.com>
2024-09-02 08:24:08 +0545

    add calculation

---
+++ calculation.py
@@ -0,0 +1,32 @@
+mark = float(input("enter your grade:"))
+
+if (mark >= 101):
+    grade = "not valid"
+    
+
+elif (mark >= 90):
+    grade = "A+"
+
+elif(mark >= 80):
+    grade = "A"
+
+elif(mark >= 70):
+    grade = "B+"
+
+elif(mark >= 60):
+    grade = "B"
+
+elif(mark >= 50):
+    grade = "C+"
+
+elif(mark >= 40):
+    grade = "C"
+
+elif(mark >= 30):
+    grade = "D"
+
+elif(mark >=20):
+    grade = "NG"
+
+
+print("grade of the sutdent ->",grade)
