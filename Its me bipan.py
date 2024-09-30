cd71ad668ef427f972dff643bd78922c0b1487c9
bipansamal <bipanmagar69@gmail.com>
2024-09-30 16:49:31 +0545

    Update Its me bipan.py

--- Its me bipan.py
+++ Its me bipan.py
@@ -1,29 +1,3 @@
-# import turtle as tur
-# import colorsys as cs
-
-# tur.setup(800,800)
-# tur.speed(0)
-# tur.tracer(10)
-# tur.width(2)
-# tur.bgcolor("black")
-# for j in range(25):
-#     for i in range(15):
-#         tur.color(cs.hsv_to_rgb(i/15,j/25,1))
-#         tur.right(90)
-#         tur.circle(200-j*4,90)
-#         tur.left(90)
-#         tur.circle(200-j*4,90)
-#         tur.right(180)
-#         tur.circle(50,24)
-# tur.hideturtle()
-# tur.done()
-
-i = 1
-n = input("enter your number")
-while i <= 10:
-    print(i*n)
-    i += 1
-
 i = 1
 n = int(input("enter the number"))
 while i <= 10:
