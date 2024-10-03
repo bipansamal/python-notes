993e9746d16e4864eea4b916cc03c8fa5d515fd5
bipansamal <bipanmagar69@gmail.com>
2024-10-03 18:03:45 +0545

    Update bipan.py

--- bipan.py
+++ bipan.py
@@ -1,19 +1,4 @@
-# import turtle
-# import colorsys
-# t = turtle. Turtle()
-# s = turtle. Screen().bgcolor('black')
-# t.speed(0)
-# n = 70
-# h = 0
-# for i in range (72):
-#     c = colorsys.hsv_to_rgb(h, 1, 0.7)
-#     h+= 1/n
-#     t.color(c)
-#     t.left(1)
-#     t.fd(1)
-#     for j in range (2):
-#         t.left(2)
-#         t.circle(100)
+
 #  multiplication table by using while loop.
 i = 1
 n = int(input("enter your number"))
@@ -22,18 +7,18 @@ while i <= 10:
     i += 1
 
 # multiplication table by using for loop.
-# n = int(input("enter the number"))
-# for i in range(1, 11):
-#     print(i*n)
+n = int(input("enter the number"))
+for i in range(1, 11):
+    print(i*n)
 
 # odd number of 10. by using for loop.
-# for i in range(1,10,2):
-#     print(i)
+for i in range(1,10,2):
+    print(i)
 
 # # even number of 10. by using for loop.
-# for i in range(2,10,2):
-#     print(i)
-#     continue
-# print(i)
-# i == 10
+for i in range(2,10,2):
+    print(i)
+    continue
+print(i)
+i == 10
            
